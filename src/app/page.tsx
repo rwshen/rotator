78283644706866b3134baa92ded28eb825d6b639
rwshen <rshen91@gmail.com>
2023-10-29 11:01:10 -0600

    fix next.config.js load issue for external src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       display: grid;
       justify-items: center;
     `}>
-      <Header title="Image Rotator" color="#9C92A3"/>
+      <Header title="Image Rotator" color="#9C92A3" />
       <section css={css`
       margin-top: 5%;
       grid-template-column: repeat(5, 20%);
@@ -21,17 +21,17 @@ export default function Home() {
           grid-column: 2 / span 3;
           grid-row: 3 / span 3;
         `}
-        width={100} height={500}/>
+          width={100} height={500} />
         <MainImage css={css`
           grid-column: 3/span 4;
           grid-row: 3/span 4;
         `}
-        width={500} height={500} />
+          width={500} height={500} />
         <RightArrow css={css`
         grid-column: 5/span 3;
         grid-row: 3/span 4;
         `}
-        width={100} height={500} />
+          width={100} height={500} />
       </section>
     </main>
   )
--- src/components/MainImage.tsx
+++ src/components/MainImage.tsx
@@ -1,12 +1,16 @@
 "use client";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import  Image from 'next/image'
 
 export function MainImage(props: { width: number, height: number }) {
+    const imgSrc = ['placekitten.com/g/', 'loremflickr.com/'];
+    
     return (
-        <img css={css`
+        <Image css={css`
             border-radius: 30px;
-            `} src={`http://placekitten.com/g/${props.width}/${props.height}`} 
+            `} 
+            src={`http://${imgSrc[1]}${props.width}/${props.height}`} 
             alt="Placeholder kitten is saving the day" 
             width={props.width} 
             height={props.height} />
